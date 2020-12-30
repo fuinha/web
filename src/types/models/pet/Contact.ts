faf2678cba50501b2cbaa5fb3baf90037aeaca50
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-29 21:30:26 -0500

    contact model

---
+++ src/types/models/pet/Contact.ts
@@ -0,0 +1,33 @@
+import { VisibleValue, AsInterface } from '#types/util'
+type ContactLevel = 'primary' | 'secondary'
+
+export interface IContact {
+	address: VisibleValue<string>
+	email: VisibleValue<string>
+	level: ContactLevel
+	name: VisibleValue<string>
+	phoneNumber: VisibleValue<string>
+}
+
+export class Contact implements IContact, AsInterface<IContact> {
+	address: VisibleValue<string>
+
+	email: VisibleValue<string>
+
+	level: ContactLevel
+
+	name: VisibleValue<string>
+
+	phoneNumber: VisibleValue<string>
+
+	constructor(contact: IContact) {
+		Object.assign(this, contact)
+	}
+
+	public asInterface(): IContact {
+		return {
+			...this,
+			level: this.level as string,
+		} as IContact
+	}
+}
