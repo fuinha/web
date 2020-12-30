fd9e3616c1b4892aec0288cdc25e6b0c47d7c2f1
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-29 21:30:24 -0500

    reminder model

---
+++ src/types/models/pet/Reminder.ts
@@ -0,0 +1,40 @@
+import { AsInterface } from '#types/util'
+type ReminderFrequency = 'weekly' | 'daily' | 'hourly'
+type ReminderNotificationMethod = 'email' | 'notification'
+
+export interface IReminder {
+	date: Date
+	enabled: boolean
+	frequency: ReminderFrequency
+	notificationMethod: ReminderNotificationMethod
+	time: string
+}
+
+export class Reminder implements IReminder, AsInterface<IReminder> {
+	date: Date
+
+	enabled: boolean
+
+	frequency: ReminderFrequency
+
+	notificationMethod: ReminderNotificationMethod
+
+	time: string
+
+	constructor(reminder: IReminder) {
+		Object.assign(this, reminder)
+	}
+
+	public toggle(): boolean {
+		this.enabled = !this.enabled
+		return this.enabled
+	}
+
+	public asInterface() {
+		return {
+			...this,
+			frequency: this.frequency as string,
+			notificationMethod: this.notificationMethod as string,
+		} as IReminder
+	}
+}
