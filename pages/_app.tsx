3718e3c72ff0a67b1f21b8bfa9dfb08fad7f1631
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-29 22:07:57 -0500

    add animateprescence

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,22 +6,25 @@ import {
 	extendTheme,
 	localStorageManager,
 } from '@chakra-ui/core'
+import { AnimatePresence } from 'framer-motion'
 import nookies from 'nookies'
 import AuthProvider from '../src/shared/auth/context'
 
 const App = ({ Component, pageProps }) => (
-	<ChakraProvider colorModeManager={localStorageManager}>
-		<ColorModeProvider
-			options={{
-				initialColorMode: 'light',
-			}}>
-			<AuthProvider>
-				<Container>
-					<Component {...pageProps} />
-				</Container>
-			</AuthProvider>
-		</ColorModeProvider>
-	</ChakraProvider>
+	<AnimatePresence exitBeforeEnter>
+		<ChakraProvider colorModeManager={localStorageManager}>
+			<ColorModeProvider
+				options={{
+					initialColorMode: 'light',
+				}}>
+				<AuthProvider>
+					<Container>
+						<Component {...pageProps} />
+					</Container>
+				</AuthProvider>
+			</ColorModeProvider>
+		</ChakraProvider>
+	</AnimatePresence>
 )
 
 export default App
