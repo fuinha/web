01482bd52eef6d02ca4e1e57385ba659493ada1a
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-29 21:27:26 -0500

    Base pages for Next

---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import {
+	ChakraProvider,
+	ColorModeProvider,
+	Container,
+	cookieStorageManager,
+	extendTheme,
+	localStorageManager,
+} from '@chakra-ui/core'
+import nookies from 'nookies'
+import AuthProvider from '../src/shared/auth/context'
+
+const App = ({ Component, pageProps }) => (
+	<ChakraProvider colorModeManager={localStorageManager}>
+		<ColorModeProvider
+			options={{
+				initialColorMode: 'light',
+			}}>
+			<AuthProvider>
+				<Container>
+					<Component {...pageProps} />
+				</Container>
+			</AuthProvider>
+		</ColorModeProvider>
+	</ChakraProvider>
+)
+
+export default App
