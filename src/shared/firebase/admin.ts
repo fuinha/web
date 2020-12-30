1fa2026f798ed6ed9e155ad83271585cbc95d669
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-29 21:46:41 -0500

    admin

--- src/shared/firebase/admin.ts
+++ src/shared/firebase/admin.ts
@@ -2,14 +2,12 @@ import firebaseAdmin from 'firebase-admin'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-const sa = process.env.adminSecret
-
 if (!firebaseAdmin.apps.length) {
 	firebaseAdmin.initializeApp({
 		credential: firebaseAdmin.credential.cert(
 			JSON.parse(process.env.adminSecret)
 		),
-		databaseURL: 'https://tfss-fbla.firebaseio.com',
+		databaseURL: process.env.NEXT_PUBLIC_databaseURL,
 	})
 }
 
