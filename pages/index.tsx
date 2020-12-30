d2245820e38f62d39265aa6e29dbdbfafb4a348b
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-29 21:27:36 -0500

    More pages

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import { Box, Button, Text } from '@chakra-ui/core'
+import { NextPage } from 'next'
+import firebase from '../src/shared/firebase/client'
+import useAuth from '../src/shared/auth/hooks/index'
+
+const Home: NextPage = () => {
+	const { loggedIn, user, signInWithGoogle, signOut } = useAuth()
+	return (
+		<Box>
+			{loggedIn ? (
+				<Text>Hello, {user.displayName}</Text>
+			) : (
+				<Text>Not Signed In.</Text>
+			)}
+			<Button onClick={loggedIn ? signOut : signInWithGoogle} />
+		</Box>
+	)
+}
+
+export default Home
